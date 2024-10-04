7c34494dbd6db669b83435ecf07a46f97bd04bfa
Robinson Vatam <robinson.vatam@protonmail.com>
2024-10-04 12:22:30 -0300

    Exercicio 1

---
+++ IdadeMaiorouMenor.py
@@ -0,0 +1,11 @@
+idade = int(input("Digite a sua idade "))
+
+if idade < 18:
+    print("Você é menor de idade")
+
+elif idade > 65:
+    print("Você é idoso")
+    
+else: 
+    print("Você é adulto e não é idoso")
+    
