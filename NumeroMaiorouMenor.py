825221f59875c28807394978d27271f6d168ec27
Robinson Vatam <robinson.vatam@protonmail.com>
2024-10-04 12:58:27 -0300

    exercicio 2

---
+++ NumeroMaiorouMenor.py
@@ -0,0 +1,13 @@
+# Solicita 2 números ao usuario.
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+# Compara os números e da uma resposta apropriada.
+if num1 == num2:
+    print("Os números são iguais!")
+
+elif num1 > num2:
+    print("O primeiro número: ", num1, "é o maior!")
+
+else:
+    print("O segundo número: ", num2, "é o maior!")
